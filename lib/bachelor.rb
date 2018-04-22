9b30676988b87a9044c230531b3968edb44dcfff
Faraz Hanif <hfaraz745@gmail.com>
2018-04-22 01:40:57 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -59,7 +59,6 @@ def get_average_age_for_season(data, season)
   data[season].each do |person|
     person.each do |k, v|
       if k == "age"
-        binding.pry
         age += v.to_f
       end
     end
